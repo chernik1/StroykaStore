60d3a6f9494a624d00180b445dbed9cd3e827a9d
chernik1 <vvvvf-vvv@mail.ru>
2024-02-16 23:33:12 +0300

    first commit

---
+++ mysite/myapp/static/js/index.js
@@ -0,0 +1,49 @@
+(function updateCatalogBehavior() {
+  let catalogLink = document.querySelector(".header__catalog-link");
+  let catalogLogo = document.querySelector(".header__catalog-logo");
+
+  catalogLink.addEventListener("mouseover", function() {
+    catalogLogo.src = "/static/img/svg/catalog-icon-white.svg";
+  });
+
+  catalogLink.addEventListener("mouseout", function() {
+    catalogLogo.src = "/static/img/svg/icon-catalog.svg";
+  });
+
+  catalogLink.addEventListener("click", function() {
+    catalogLogo.src = "/static/img/svg/catalog-icon-grey.svg";
+  });
+})();
+
+window.addEventListener('scroll', function() {
+    const mainTools = document.querySelector('.main__tools');
+    if (window.scrollY > 100) {
+        mainTools.style.position = 'fixed';
+        mainTools.style.top = '0';
+        mainTools.style.left = '0';
+        mainTools.style.right = '0';
+        mainTools.maxWidth = '2040px';
+    } else {
+        mainTools.style.position = 'static';
+    }
+});
+
+function openModal() {
+  let btn = document.querySelector(".header__location_button");
+  let modal = document.querySelector("#modal__window_city");
+  let overlay = document.querySelector(".modal__window");
+
+  btn.addEventListener("click", function() {
+    modal.classList.add("modal-open");
+    overlay.classList.add("modal-open");
+  });
+
+  let closeButton = document.querySelector(".modal__window-close");
+  closeButton.addEventListener("click", function() {
+  modal.classList.remove("modal-open");
+  overlay.classList.remove("modal-open");
+});
+}
+
+
+

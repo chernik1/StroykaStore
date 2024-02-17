e2b2bb6fe1c1af97b6ce626df36cf963931b0003
chernik1 <vvvvf-vvv@mail.ru>
2024-02-17 20:02:24 +0300

    add basket buttons

--- mysite/myapp/static/js/index.js
+++ mysite/myapp/static/js/index.js
@@ -84,8 +84,69 @@ function openModal() {
 
     rightArrow.addEventListener('click', function() {
         changeBackgroundImage("/static/img/main-tab-2.png", "Распродажа инструментов", "«СтройкаСтор» стремится сделать условия покупки максимально выгодными для каждого покупателя, поэтому на сайте регулярно появляются товары со скидкой");
-    })();
+    });
 })();
 
+(function() {
+  const baskets = document.querySelectorAll(".basket-click");
+  const quantities = document.querySelectorAll(".basket__quantity");
+  const minuses = document.querySelectorAll(".basket__minus");
+  const pluses = document.querySelectorAll(".basket__plus");
+  const inputs = document.querySelectorAll(".basket__input");
 
+  baskets.forEach((basket, index) => {
+    basket.addEventListener("click", function () {
+      basket.style.visibility = "hidden";
+      quantities[index].style.visibility = "visible";
+    });
 
+    minuses[index].addEventListener("click", function () {
+      let value = parseInt(inputs[index].value);
+      if (value > 1) {
+        value--;
+        inputs[index].value = value;
+      }
+    });
+
+    pluses[index].addEventListener("click", function () {
+      let value = parseInt(inputs[index].value);
+      value++;
+      inputs[index].value = value;
+    });
+
+    function returnBasketVisibility() {
+      basket.style.visibility = "visible";
+      quantities[index].style.visibility = "hidden";
+    }
+
+    inputs[index].addEventListener("keyup", function(event) {
+      if (event.key === "Enter") {
+        returnBasketVisibility();
+      }
+    });
+
+    minuses[index].addEventListener("keyup", function(event) {
+      if (event.key === "Enter") {
+        returnBasketVisibility();
+      }
+    });
+
+    pluses[index].addEventListener("keyup", function(event) {
+      if (event.key === "Enter") {
+        returnBasketVisibility();
+      }
+    });
+
+    minuses[index].addEventListener("keydown", function(event) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+      }
+    });
+
+    pluses[index].addEventListener("keydown", function(event) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+      }
+    });
+  });
+})();

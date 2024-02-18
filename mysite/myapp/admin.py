5de3291838372d2baa8445aa0ee156363353025e
chernik1 <vvvvf-vvv@mail.ru>
2024-02-19 01:42:44 +0300

    add db

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Brands
 
 # Register your models here.
+
+@admin.register(Brands)
+class BrandsAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from .models import *
 
 # Create your views here.
 
@@ -7,5 +8,26 @@ def index(request):
     return render(request, 'index.html')
 
 def brands(request):
-    return render(request, 'brands.html')
+
+    brands = Brands.objects.all()
+    rus_letters = []
+    eng_letters = []
+
+    for brand in brands:
+        letter = brand.name[0]
+        if letter.upper() in 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' and letter.upper() not in rus_letters:
+            rus_letters.append(letter.upper())
+        elif letter.upper() in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' and letter.upper() not in eng_letters:
+            eng_letters.append(letter.upper())
+
+    rus_letters.sort()
+    eng_letters.sort()
+
+    context = {
+        'rus_letters': rus_letters,
+        'eng_letters': eng_letters,
+        'brands': brands
+    }
+
+    return render(request, 'brands.html', context=context)
 

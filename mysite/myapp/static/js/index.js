4202eaa1f973f5971f60889cc322927a067a6db9
chernik1 <vvvvf-vvv@mail.ru>
2024-02-18 12:02:46 +0300

    add base js

--- mysite/myapp/static/js/index.js
+++ mysite/myapp/static/js/index.js
@@ -16,22 +16,6 @@
   });
 })();
 
-// Фиксированная шапка
-(function fixedHeader() {
-    window.addEventListener('scroll', function() {
-    const mainTools = document.querySelector('.main__tools');
-    if (window.scrollY > 100) {
-        mainTools.style.position = 'fixed';
-        mainTools.style.top = '0';
-        mainTools.style.left = '0';
-        mainTools.style.right = '0';
-        mainTools.maxWidth = '2040px';
-    } else {
-        mainTools.style.position = 'static';
-    }
-});
-})();
-
 // Модальное окно
 function openModal() {
   let btn = document.querySelector(".header__location_button");
@@ -167,40 +151,3 @@ function openModal() {
   });
 })();
 
-(function () {
-
-  const smoothScroll = function (targetEl, duration) {
-      let target = document.querySelector(targetEl);
-      let targetPosition = target.getBoundingClientRect().top - 200;
-      let startPosition = window.pageYOffset;
-      let startTime = null;
-
-      const ease = function(t, b, c, d) {
-          t /= d / 2;
-          if (t < 1) return c / 2 * t * t + b;
-          t--;
-          return -c / 2 * (t * (t - 2) - 1) + b;
-      };
-
-      const animation = function(currentTime) {
-          if (startTime === null) startTime = currentTime;
-          const timeElapsed = currentTime - startTime;
-          const run = ease(timeElapsed, startPosition, targetPosition, duration);
-          window.scrollTo(0, run);
-          if (timeElapsed < duration) requestAnimationFrame(animation);
-      };
-      requestAnimationFrame(animation);
-  };
-
-  const scrollTo = function () {
-      const links = document.querySelectorAll('.js-scroll');
-      links.forEach(each => {
-          each.addEventListener('click', function (e) {
-              e.preventDefault();
-              const currentTarget = this.getAttribute('href');
-              smoothScroll(currentTarget, 1000);
-          });
-      });
-  };
-  scrollTo();
-}());
